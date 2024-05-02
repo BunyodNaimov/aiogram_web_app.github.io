55b43da5322cd94fc16c00362f0c9530f9a089ee
bunyod <naimov.bunyod2021@gmail.com>
2024-05-02 10:35:04 +0500

    update project

--- bot/keyboards.py
+++ bot/keyboards.py
@@ -1,6 +1,6 @@
 from aiogram.types import WebAppInfo, ReplyKeyboardMarkup, KeyboardButton
 
-webapp = WebAppInfo(url='https://bunyodnaimov.github.io/aiogram_web_app.github.io/.')
+webapp = WebAppInfo(url='https://bunyodnaimov.github.io/aiogram_web_app.github.io/')
 
 site_kb = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text='Site', web_app=webapp)],
