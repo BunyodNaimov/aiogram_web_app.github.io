86d0c4877425dc94c847cb81d745233803dddc27
bunyod <naimov.bunyod2021@gmail.com>
2024-05-04 14:34:03 +0500

    update project

--- t_bot/handlers.py
+++ t_bot/handlers.py
@@ -1,4 +1,3 @@
-
 from aiogram import types, Dispatcher
 
 from aiogram.filters import CommandStart, Command
@@ -7,6 +6,14 @@ from keyboards import site_kb
 from t_bot.db import db_get_all_products
 
 dp = Dispatcher()
+PRICE = {
+    '1': [types.LabeledPrice(label='Item1', amount=100000)],
+    '2': [types.LabeledPrice(label='Item2', amount=200000)],
+    '3': [types.LabeledPrice(label='Item3', amount=300000)],
+    '4': [types.LabeledPrice(label='Item4', amount=400000)],
+    '5': [types.LabeledPrice(label='Item5', amount=500000)],
+    '6': [types.LabeledPrice(label='Item6', amount=600000)]
+}
 
 
 @dp.message(CommandStart())
