ef2bcd98264db0c9cd7f7057819cbbb6a139415f
bunyod <naimov.bunyod2021@gmail.com>
2024-05-09 17:50:11 +0500

    fastapi

--- t_bot/keyboards.py
+++ t_bot/keyboards.py
@@ -1,6 +1,6 @@
 from aiogram.types import WebAppInfo, ReplyKeyboardMarkup, KeyboardButton
 
-webapp = WebAppInfo(url='https://ab12-178-218-201-42.ngrok-free.app/')
+webapp = WebAppInfo(url='https://77d8-178-218-201-42.ngrok-free.app/')
 
 site_kb = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text='Site', web_app=webapp)],
