2b5842b0c4a3bd9d1cdb9e0ee8e867833c0530a9
bunyod <naimov.bunyod2021@gmail.com>
2024-05-11 09:04:39 +0500

    fastapi

--- static/app.js
+++ static/app.js
@@ -7,7 +7,26 @@ tg.MainButton.color = '#2cab37';
 
 let r_name = ""
 let r_price = ""
+
+
+let rmv_btn = document.getElementById("rmv_btn");
+let add_btn = document.getElementById("add_btn");
+let num = document.getElementById("num");
+let count_num = 0;
+
+
+add_btn.addEventListener("click", function () {
+    num.innerText = count_num += 1;
+})
+rmv_btn.addEventListener("click", function () {
+    num.innerText = count_num -= 1;
+})
+
 function func(name, price) {
+    add_btn.style.display = "none";
+    add_btn.style.display = "inline-block";
+    rmv_btn.style.display = "inline-block";
+    num.style.display = "inline-block";
     tg.MainButton.setText(name);
     r_name = name;
     r_price = price;
